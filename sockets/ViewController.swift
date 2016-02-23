ffd1a71fbb292441ce9c7faa1f24f49fc771cd9f
JeSchmuck <levaya_post@mail.ru>
2016-02-23 23:51:30 +0300

    Initial Commit

---
+++ sockets/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sockets
+//
+//  Created by Zakhar Rudenko on 23.02.16.
+//  Copyright © 2016 Zakhar Rudenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
